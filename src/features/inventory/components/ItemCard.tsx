06ca47429506f76c5a42d7723da66d046bf79cd4
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2026-03-28 00:06:18 +0530

    feat(inventory): Implemented shadcn/ui in ItemCard component

--- src/features/inventory/components/ItemCard.tsx
+++ src/features/inventory/components/ItemCard.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
 type Props = {
     item: any;
     onBorrow: (id: string) => void;
@@ -5,16 +8,29 @@ type Props = {
 
 export default function ItemCard({item, onBorrow}: Props) {
     return (
-        <div>
-            <p>{item.name}</p>
-            <p>{item.available_quantity}</p>
-
-            <button 
-                onClick={() => onBorrow(item.id)}
-                disabled={item.available_quantity === 0}
-            >
-                Borrow
-            </button>
-        </div>
+        <Card className="relative mx-auto w-full max-w-sm pt-0">
+            <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+            <img
+                src="https://avatar.vercel.sh/shadcn1"
+                alt="Event cover"
+                className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+            />   
+            <CardHeader>
+                <CardTitle className="text-lg">{item.name}</CardTitle>
+                <CardDescription className="text-lg">
+                    {item.available_quantity} / {item.total_quantity}
+                </CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <Button     
+                    onClick={() => onBorrow(item.id)}
+                    disabled={item.available_quantity === 0}
+                    className="w-full h-10 text-lg"
+                >
+                    Borrow
+                </Button>
+            </CardFooter>
+            
+        </Card>
     )
 }
