6d6f84c57c746def12a20f445cf34cb7d9de8bc0
Aditeya J Frankur <88970191+aditeya24@users.noreply.github.com>
2026-03-29 07:42:14 +0000

    Revert "Inventory UI"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import InventoryPage from "./features/inventory/pages/InventoryPage";
+import { useEffect, useState } from "react";
+import { supabase } from "./services/supabase";
 
 function App() {
-  return <InventoryPage />;
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const { data } = await supabase.from("components").select("*");
+    if (data) setData(data);
+  }
+
+  return (
+    <div>
+      <h1>Inventoria</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.available_quantity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
