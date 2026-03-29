5b1962c44df407ff92df309de07bf93913059524
Ramu-the-noob <adityasaji2006@gmail.com>
2026-03-29 19:20:36 +0530

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // It's highly recommended to use environment variables for these
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://whxjskmopgipxzxfskbl.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_fW7xCqVd6yXV5GTT6ZO85A_n5Qr1jHf'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
